eba9376c413bba1b16e57bbd4cab2d02b4a4f0bc
Thanh1999 <t.max347056@gmail.com>
2024-02-03 23:24:45 +0700

    delete unsuded file

--- abs.js
+++
@@ -1,9 +0,0 @@
-if (context.payload.pull_request.body === null) {
-    github.rest.pulls.update({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      body: `Your pull request access link will be https://${{env.S3-BUCKET}}.s3.us-east-1.amazonaws.com/
-            + '\n' + For example: https://${{env.S3-BUCKET}}.s3.us-east-1.amazonaws.com/images/logos/lastv.png
-            + '\n' + context.payload.pull_request.body`
-    })
-  }
